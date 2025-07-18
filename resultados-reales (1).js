647460b1714f4982794b42c4a38c8168a1e954de
OkeyCRM <deleonleonel029@gmail.com>
2025-07-18 16:35:15 -0300

    Delete resultados-reales (1).js

--- resultados-reales (1).js
+++
@@ -1,7 +0,0 @@
-
-// resultados-reales.js
-// Actualmente no requiere funcionalidad JS activa, pero dejamos la estructura
-
-document.addEventListener("DOMContentLoaded", function () {
-    console.log("Resultados Reales cargado correctamente.");
-});
